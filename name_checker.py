8c02703da41790cce80c001c8cc6bcd06459fe48
ooooda <anya.smirdina@gmail.com>
2021-03-20 16:12:46 +0300

    Added a checker for existing comments about commits

--- name_checker.py
+++ name_checker.py
@@ -6,15 +6,11 @@ TOKEN = 'token is hidden'
 USER = 'ooooda'
 REPOSITORY = 'python_au'
 
-
 def get_header():
-    headers = {'Authorization': 'token ' + TOKEN}
-    return headers
-
+    return {'Authorization': 'token ' + TOKEN}
 
 def get_prefix():
     return 'https://api.github.com/repos/' + USER + '/' + REPOSITORY
-
 
 def check_all_pulls(state):
     response = requests.get(get_prefix() + '/pulls?state=' + state, headers=get_header())
@@ -32,44 +28,64 @@ def check_all_commits(response, pull):
     commits = response.json()
     if response.status_code != 200:
         raise ValueError(commits)
+    comments = requests.get(get_prefix() + '/issues/' + str(pull['number']) + '/comments', headers=get_header()).json()
+    comment_time = None
+    if len(comments) != 0:
+        comment_time = comments[-1]['created_at']
     for commit in commits:
+        if comment_time is not None:
+            if comment_time >= commit['commit']['author']['date']:
+                continue
         commit_checker(commit['commit']['message'], pull)
 
 
 def commit_checker(commit, pull):
-    part1 = commit.split(" ")[0]
-    print(part1)
-    part2 = "".join(commit.split(" ")[1:])
-    if not re.match(r"(Added |Deleted |Fixed )[^A-Z]*", commit):
-        message = "wrong commit format"
-        if not re.match(r"(Added|Deleted|Fixed)", part1):
-            print('!!!' + part1)
-            message += ', wrong action name'
-        if not re.match(r"([^A-Z]*)", part2):
-            message += ', wrong letters format'
+    has_mistake = False
+    message = "wrong commit format"
+    name = pull['title']
+    if '-' not in name:
         send_message(pull, message)
-        print(commit + '- ' + message)
-
+        print(name + ' - ' + message)
+        return
+    name = name.split('-', maxsplit=1)
+    task_name = name[0]
+    group_number = name[1]
+    if not re.match(r"(HW[0-4]|LEETCODE)", task_name):
+        message += ", wrong task name"
+        has_mistake = True
+    if not re.match(r"1021", group_number):
+        message += ", wrong group number"
+        has_mistake = True
+    if has_mistake:
+        send_message(pull, message)
+        print(pull['title'] + ' - ' + message)
 
 def pull_checker(pull):
-    part1 = pull['title'].split('-')[0]
-    part2 = "".join(pull['title'].split('-')[1:])
-    if not re.match(r"(HW[0-4]|LEETCODE)-1021", pull['title']):
-        message = "wrong pull request format"
-        if not re.match(r"(HW[0-4]|LEETCODE)", part1):
-            message += ', wrong title name'
-        if not re.match(r"1021", part2):
-            message += ', wrong group number format'
+    has_mistake = False
+    message = "wrong pull request format"
+    name = pull['title']
+    if '-' not in name:
+        print('happened', name)
         send_message(pull, message)
-        print(pull['title'] + '- ' + message)
-
+        print(name + ' - ' + message)
+        return
+    name = name.split('-', maxsplit=1)
+    task_name = name[0]
+    group_number = name[1]
+    if not re.match(r"(HW[0-4]|LEETCODE)", task_name):
+        message += ", wrong task name"
+        has_mistake = True
+    if not re.match(r"1021", group_number):
+        message += ", wrong group number"
+        has_mistake = True
+    if has_mistake:
+        send_message(pull, message)
+        print(pull['title'] + ' - ' + message)
 
 def send_message(pull, message):
     body = {'body': message}
-    response = requests.post(get_prefix() + '/issues/' + str(pull['number']) + '/comments', headers=get_header(),
-                             data=json.dumps(body))
+    response = requests.post(get_prefix() + '/issues/' + str(pull['number']) + '/comments', headers=get_header(), data=json.dumps(body))
     if response.status_code != 201:
         print(response)
-
 
 check_all_pulls(state='all')
